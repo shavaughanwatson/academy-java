93d63b71847afd8080571bd7b29a58b9ce5800ea
shavaughan watson <shavaughan.j.watson@gmail.com>
2024-09-25 17:01:12 -0400

    edited file

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -3,6 +3,38 @@ package com.bptn.course._05_strings;
 public class Strings {
 
 	public static void main(String[] args) {
+		// to create a new String 
+		String s = "Hello World";
+		
+		int stringLength = s.length();
+		
+		System.out.println("The length of the string is: " +stringLength);
+		
+		
+		String sub = s.substring(0, 5);
+		
+		System.out.println("The substring with start index 0 and end index 5 "+ sub);
+		
+		String sub1 = s.substring(3);
+		
+		System.out.println("The substring with start index 3" + sub1);
+		
+		String lowerCase = s.toLowerCase();
+		
+		System.out.println("The lowercase of the with string " + lowerCase);
+		
+		String UpperCase = s.toUpperCase();
+		
+		System.out.println("The uppercase of the with string " + UpperCase);
+		
+		int indexOf = s.indexOf("World");
+		
+		System.out.println("The index of World is:" + indexOf);
+		
+		//Find the character
+		char c = s.charAt(0);
+		
+		System.out.print(c);
 		
 	}
 }
