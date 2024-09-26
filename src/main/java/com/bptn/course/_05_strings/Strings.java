6677af33521c08d2eab73beada3df30ea13f3cea
shavaughanwatson <shavaughan.j.watson@gmail.com>
2024-09-25 23:53:23 -0400

    changed file

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -31,10 +31,21 @@ public class Strings {
 		
 		System.out.println("The index of World is:" + indexOf);
 		
-		//Find the character
+		//Find the character at the speciefed index -0
 		char c = s.charAt(0);
 		
 		System.out.print(c);
 		
+		
+		//Find the character at the speciefed index -3
+		char cl = s.charAt(3);
+		
+		String reversedWorld;
+		
+		for (int i= s.length()-1 ; i >=0 ; i--) {
+			reversedWorld += s.charAt(i);
+		}
+		
+		System.out.println(reversedWorld);
 	}
 }
